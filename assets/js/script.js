96bd028183839d8f24ad05ea3b381bc54bc2e8f3
Gabriel Torres <playerjogador10@outlook.com>
2022-08-17 16:09:35 -0300

    smooth scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,3 +56,59 @@ setInterval(() => {
     contador = 0;
   }
 }, x);
+
+// smooth scrolling
+
+const menuLinks = document.querySelectorAll('.header-menu a[href^="#"]');
+const gt = document.querySelector(".logo");
+
+function pegaDistanciaDoTopo(element) {
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+// function nativeScroll(pegaDistanciaDoTopo) {
+//   window.scroll({
+//     top: pegaDistanciaDoTopo,
+//     behavior: "smooth",
+//   });
+// }
+
+function scrollTo(event) {
+  event.preventDefault();
+  const distanciaDoTopo = pegaDistanciaDoTopo(event.target) - 50;
+  smoothScrollTo(0, distanciaDoTopo);
+}
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", scrollTo);
+});
+
+gt.addEventListener("click", scrollTo);
+
+//funcao pré-definida para a smooth scroll funcionar no safari
+function smoothScrollTo(endX, endY, duration) {
+  const startX = window.scrollX || window.pageXOffset;
+  const startY = window.scrollY || window.pageYOffset;
+  const distanceX = endX - startX;
+  const distanceY = endY - startY;
+  const startTime = new Date().getTime();
+
+  duration = typeof duration !== "undefined" ? duration : 400;
+
+  const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1)
+      return (distance / 2) * time * time * time * time + from;
+    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
+    }
+    window.scroll(newX, newY);
+  }, 1000 / 60);
+}
